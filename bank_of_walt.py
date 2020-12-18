49f5bf0636d1029ebba0b68475bd16bc585b8b8c
Hannah Provenza <hprovenza@gmail.com>
2020-12-18 16:49:00 -0500

    initial commit

---
+++ bank_of_walt.py
@@ -0,0 +1,115 @@
+# This example requires the 'members' privileged intents
+
+import discord
+from discord.ext import commands
+import json
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        print(f"adding {user.id}")
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['bux'] = 100
+
+
+@bot.command()
+async def bal(ctx):
+    """Check the balance of your waltbux account."""
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, ctx.author)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+    await ctx.send(f'{ctx.author} has {users[f"{ctx.author.id}"]["bux"]} bux in their walt vault')
+
+
+@bot.command()
+async def give(ctx, member: discord.Member, n: int):
+    """Give your waltbux to another user."""
+    if n > 0:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, ctx.author)
+
+        if ctx.author.id != member.id:
+            if users[f"{ctx.author.id}"]['bux'] >= n:
+                users[f"{ctx.author.id}"]['bux'] -= n
+                users[f"{member.id}"]['bux'] += n
+                await ctx.send(f'Gave {n} waltbux to {member}')
+            else: await ctx.send("You don't have enough waltbux to make that gift!")
+        else: await ctx.send("You can't give yourself waltbux!")
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+@bot.command()
+async def bet(ctx, arbiter: discord.Member, n: int, *condition: str):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    if ctx.author.id != arbiter.id:
+        if users[f"{ctx.author.id}"]['bux'] >= n:
+            await ctx.send(f"BET: {ctx.author} {n} {' '.join(condition)} awaiting judgement from {arbiter}")
+        else: await ctx.send("You don't have enough waltbux to make that bet!")
+    else: await ctx.send("You can't resolve your own bet!")
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if reaction.message.content.startswith("BET:"):
+        msg = reaction.message.content.split()
+        arbiter = msg[-1]
+        if user == reaction.message.guild.get_member_named(arbiter):
+
+            if reaction.emoji == '✅':
+                with open('users.json', 'r') as f:
+                    users = json.load(f)
+                bettor = reaction.message.guild.get_member_named(msg[1])
+                amount = int(msg[2])
+                users[f"{bettor.id}"]["bux"] += amount
+                with open('users.json', 'w') as f:
+                    json.dump(users, f)
+
+            elif reaction.emoji == '❌':
+                with open('users.json', 'r') as f:
+                    users = json.load(f)
+                bettor = reaction.message.guild.get_member_named(msg[1])
+                amount = int(msg[2])
+                users[f"{bettor.id}"]["bux"] -= amount
+                with open('users.json', 'w') as f:
+                    json.dump(users, f)
+
+
+bot.run(bot_token)
